16be6ce6b804271c04c0a2716f47816e4c054a02
PocketBrain <tim.kirp@mail.ru>
2024-11-27 17:16:30 +0300

    minor fixes

--- services/ml.py
+++ services/ml.py
@@ -1,45 +1,25 @@
-import tempfile
-
 import torch
+import base64
+from io import BytesIO
+from typing import List, Dict, Any
+from ml.lifespan import device, imagebind_model
 from imagebind.model import ModalityType
 from imagebind.utils import data
 
-from ml.lifespan import device, imagebind_model
+from repositories.documents import DocumentRepository
+from schemas.documnet import CreateDocumentOpts
+from ml.utils import DocxProcessor, PdfProcessor
 
 
 class MlService:
     def __init__(self):
         self._imagebind_model = imagebind_model
-
         self.device = device
+        self.document_repository = DocumentRepository()
 
     def _extract_text_embedding(self, text: str) -> torch.Tensor:
         """
         Извлекает эмбеддинг для текста.
-
-        Параметры
-        ----------
-        text : str
-            Входной текст для извлечения эмбеддинга. Если строка пустая или не является строкой,
-            будет использовано значение по умолчанию "<UNK>".
-
-        Возвращает
-        -------
-        torch.Tensor
-            Эмбеддинг текста, полученный с помощью модели ImageBind.
-
-        Примечания
-        ---------
-        Функция использует метод `load_and_transform_text` для предварительной обработки текста,
-        приводя его в формат, подходящий для обработки моделью. Затем эмбеддинг извлекается
-        в режиме inference, что позволяет выполнять вычисления без сохранения промежуточных
-        данных для обучения.
-
-        Исключения
-        ---------
-        Проверка типа входных данных выполняется с целью обеспечения безопасности и предотвращения
-        ошибок при передаче некорректного формата. Если переданный текст не соответствует
-        ожидаемому типу, используется placeholder "<UNK>".
         """
         if not isinstance(text, str) or not text:
             text = "<UNK>"
@@ -48,14 +28,40 @@ class MlService:
             text_embedding = self._imagebind_model(inputs)[ModalityType.TEXT]
         return text_embedding
 
-    def _extract_image_embeddings(self, image: bytes):
-        with tempfile.NamedTemporaryFile(delete=True) as temp:
-            temp.write(image)
-            inputs = {
-                ModalityType.VISION: data.load_and_transform_vision_data(
-                    [temp.name], self.device
-                )
+    def process_and_store_document(self, file_bytes: bytes) -> None:
+        """
+        Обрабатывает документ, векторизует чанки и сохраняет их в Qdrant.
+        :param file_bytes: Содержимое .docx файла в байтах.
+        """
+        docx_processor = DocxProcessor()
+        chunks = list(docx_processor.process(file_bytes))
+        documents = []
+
+        for chunk in chunks:
+            text = chunk['chunk']
+            metadata = chunk['metadata']
+
+            embedding = self._extract_text_embedding(text).squeeze().tolist()
+
+            images_metadata = [
+                {
+                    "position": img.get("position"),
+                }
+                for img in metadata.get('images', [])
+            ]
+
+            metadata_to_store = {
+                "text": text,
+                "start_word": metadata.get("start_word"),
+                "end_word": metadata.get("end_word"),
+                "images": images_metadata,
             }
-            with torch.inference_mode():
-                vision_embedding = self._imagebind_model(inputs)[ModalityType.TEXT]
-            return vision_embedding
+
+            document_opt = CreateDocumentOpts(
+                vector=embedding,
+                metadata=metadata_to_store
+            )
+
+            documents.append(document_opt)
+
+        self.document_repository.create_document(documents)
