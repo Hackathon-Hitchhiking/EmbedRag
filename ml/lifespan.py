a5e01e8cedbe02013dde46864c11d5b223f49996
PocketBrain <tim.kirp@mail.ru>
2024-11-27 17:15:10 +0300

    add llm

--- ml/lifespan.py
+++ ml/lifespan.py
@@ -1,6 +1,9 @@
 import torch
 import imagebind
 from loguru import logger
+from llm import LLama3Quantized
+from config import ModelKwargs
+from constants import LLM_PATH
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -8,3 +11,17 @@ logger.debug("loading imagebind")
 imagebind_model = imagebind.model.imagebind_huge(True)
 imagebind_model.eval()
 imagebind_model.to(device)
+
+
+llm = LLama3Quantized()
+
+
+kwargs = ModelKwargs(
+    temperature=0.7,
+    top_k=30,
+    top_p=0.9,
+    max_tokens=8192,
+    repeat_penalty=1.1,
+)
+
+llm.load_model(kwargs, LLM_PATH)
